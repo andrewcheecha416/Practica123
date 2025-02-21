c8ed8f6ce2930adce2c9197679c3c3f031014e72
andrewcheecha416 <andrii.buhir@alumnos.colegiosma.com>
2025-02-21 16:58:21 +0100

    Update EjemploJava2025.java

--- src/main/java/com/mycompany/ejemplojava2025/EjemploJava2025.java
+++ src/main/java/com/mycompany/ejemplojava2025/EjemploJava2025.java
@@ -6,6 +6,7 @@ package com.mycompany.ejemplojava2025;
 public class EjemploJava2025 {
 
     public static void main(String[] args) {
-        System.out.println("Mi nombre");
+     // System.out.println("Mi nombre");
+        (“Hola -pon aquí un nombre- escribe aquí algo más”);
     }
 }
